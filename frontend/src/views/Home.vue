67a634e1337afbe7af546f7309623a8913da8628
fuatkarakus <fuatkrks11@gmail.com>
2020-05-17 14:09:00 +0300

    footer header components

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Navbar />
+    <News />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import News from '@/components/News.vue';
+import Navbar from '@/components/Navbar.vue';
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    Navbar,
+    News
   }
 };
+
 </script>
